c11d5c2aa37aef6a378a01a294a9270261dfc768
Nilesh Prajapat <158799822+Nilesh-Prajapat@users.noreply.github.com>
2025-03-12 16:52:04 +0530

    added loading animation and background animation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:port_folio/splashscreen.dart';
 import 'package:provider/provider.dart';
 import 'package:port_folio/theme_provider.dart';
-
 import 'theme/theme.dart';
 
 void main() {
@@ -16,56 +13,8 @@ void main() {
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late Future<void> _assetsFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _assetsFuture = _loadAssets();
-  }
-
-  // Load images and fonts from assets
-  Future<void> _loadAssets() async {
-    await _preloadImages('assets/images');
-    await _preloadImages('assets/icons');
-    await _loadFonts();
-  }
-
-  // Preload images
-  Future<void> _preloadImages(String directory) async {
-    final manifestJson = await rootBundle.loadString('AssetManifest.json');
-    final Map<String, dynamic> manifest = jsonDecode(manifestJson);
-
-    final imageFiles = manifest.keys
-        .where((String key) => key.startsWith(directory) && (key.endsWith('.png') || key.endsWith('.jpg')))
-        .toList();
-
-    for (final file in imageFiles) {
-      precacheImage(AssetImage(file), context);
-    }
-  }
-
-  // Preload fonts
-  Future<void> _loadFonts() async {
-    final manifestJson = await rootBundle.loadString('AssetManifest.json');
-    final Map<String, dynamic> manifest = jsonDecode(manifestJson);
-
-    final fontFiles = manifest.keys
-        .where((String key) => key.startsWith('assets/fonts') && key.endsWith('.ttf'))
-        .toList();
-
-    for (final font in fontFiles) {
-      await rootBundle.load(font);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,7 +25,7 @@ class _MyAppState extends State<MyApp> {
       theme: lightTheme(16.0),
       darkTheme: darkTheme(16.0),
       themeMode: themeProvider.themeMode,
-      home: const SplashScreen(), // Directly show the SplashScreen
+      home: const SplashScreen(), // Directly showing the SplashScreen
     );
   }
 }
