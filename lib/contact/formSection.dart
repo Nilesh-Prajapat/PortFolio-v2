ca4ab5648a1732373a1a0e9383d1344fa8b0a4bd
Nilesh Prajapat <158799822+Nilesh-Prajapat@users.noreply.github.com>
2025-03-16 14:14:48 +0530

    dev.

--- lib/contact/formSection.dart
+++ lib/contact/formSection.dart
@@ -40,7 +40,7 @@ class _FormSectionState extends State<FormSection> {
       errorMessage = null;
     });
 
-    final Uri apiUrl = Uri.parse("https://itsnilesh.app/api/send_email"); // ✅ Your Vercel API URL
+    final Uri apiUrl = Uri.parse("https://itsnilesh.vercel.app/api/send_email"); // ✅ Your Vercel API URL
 
     try {
       final response = await http.post(
